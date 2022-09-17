9ce53ba573a8021bd06a97165e8837430c73bfc0
Norbelys Naguanagua <norbelysnaguanagua21@gmail.com>
2022-09-17 17:17:54 -0500

    cuota de ventas 50%

--- lib/@presentation/pages/login.dart
+++ lib/@presentation/pages/login.dart
@@ -208,11 +208,11 @@ class _LoginPageState extends State<LoginPage> {
         child: SizedBox(
           height: 50.0,
           width: _size.width,
-          child: new RaisedButton(
-            elevation: 5.0,
+          child: new ElevatedButton(
+            /* elevation: 5.0,
             shape: new RoundedRectangleBorder(
                 borderRadius: new BorderRadius.circular(25.0)),
-            color: Color(0xff0091CE),
+            color: Color(0xff0091CE), */
             child: Center(
               child: Text(            
                 'Iniciar Sesión',
